2149408ed2840511681f7064086257dc46770cc5
Seonghoon <pd0a6764@posco.com>
2023-07-18 16:46:29 +0900

    localstorage를 이용한 login 기능

--- app.js
+++ app.js
@@ -3,22 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
 function onLoginSubmit(event) {
     event.preventDefault();
 
-    if (localStorage.getItem("userName") === '') {
-        loginForm.classList.add(HIDDEN_CLASSNAME);
-
-        greeting.innerText = `Hello ${userName}`;
-        greeting.classList.remove(HIDDEN_CLASSNAME);
-    } else {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const userName = loginInput.value;
-    localStorage.setItem("userName", userName);
-    greeting.innerText = `Hello ${userName}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    }
-
+    localStorage.setItem(USERNAME_KEY, userName);
+    paintGreetings(userName);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(userName) {
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUserName);
+}
